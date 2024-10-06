0b615e885f41f4a4f2a845a038d6e1b6e9e3468e
Arkadiusz Zaskalski <arekzaskalski@interia.pl>
2024-10-06 11:44:57 +0200

    Tiles for powiaty added

--- js/app.js
+++ js/app.js
@@ -3,9 +3,10 @@ const map = L.map('map',{
   maxZoom:19
         });
 
-map.setView([52.00, 19.63], 6.5);
+map.setView([52.00, 19.63], 6);
 
-const wojewodztwa= L.tileLayer('tiles/wojewodztwa/{z}/{x}/{y}.png',{maxNativeZoom:9,maxZoom:10,minZoom:6,transparent:true}).addTo(map)
+const wojewodztwa= L.tileLayer('tiles/wojewodztwa/{z}/{x}/{y}.png',{maxNativeZoom:9,maxZoom:11,minZoom:6,transparent:true}).addTo(map);
+const powiaty=L.tileLayer('tiles/powiaty/{z}/{x}/{y}.png',{maxNativeZoom:11,maxZoom:13,minZoom:8,transparent:true}).addTo(map);
 
 const openStreet=L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 20,
@@ -18,6 +19,8 @@ const baseMaps = {
  'Brak': beztla};
 
 const overlayMap={
+  
+  'Powiaty':powiaty,
   'Województwa':wojewodztwa
 }
 
