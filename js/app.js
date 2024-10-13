9c727bc44009c364cb8680f2ce4155e8d77366d8
Arkadiusz Zaskalski <arekzaskalski@interia.pl>
2024-10-13 21:57:26 +0200

    Header + MyZoomBar added

--- js/app.js
+++ js/app.js
@@ -1,12 +1,35 @@
 const map = L.map('map',{
   zoomSnap: 0.5,	
-  maxZoom:19
-        });
+  maxZoom:19,
+  zoomControl:false});
 
 map.setView([52.00, 19.63], 6);
 
+//Extend ZoomBar - Adbutton "Start map"
+L.Control.MyZoomBar = L.Control.Zoom.extend({
+	onAdd: function(map) {
+				const container = L.Control.Zoom.prototype.onAdd.call(this, map);
+				// Dodaj nowy przycisk
+				const startMap = L.DomUtil.create('a', 'leaflet-control-zoom-bar', container);
+				startMap.innerHTML = '<img src="css/images/home.png" style="margin-top:2px">';
+				startMap.href = '#';
+				startMap.title = 'Mapa startowa';
+				L.DomEvent.on(startMap, 'click', this._zoomToStart, this);
+				container.prepend(startMap);
+				return container;
+			},
+	_zoomToStart: function(e) {
+        L.DomEvent.stopPropagation(e);
+        L.DomEvent.preventDefault(e);
+        map.setView([52.00, 19.63], 6);
+    }
+});
+	
+map.addControl(new L.Control.MyZoomBar())
+
+
 const wojewodztwa= L.tileLayer('tiles/wojewodztwa/{z}/{x}/{y}.png',{maxNativeZoom:9,maxZoom:11,minZoom:6,transparent:true}).addTo(map);
-const powiaty=L.tileLayer('tiles/powiaty/{z}/{x}/{y}.png',{maxNativeZoom:11,maxZoom:13,minZoom:8,transparent:true}).addTo(map);
+const powiaty=L.tileLayer('tiles/powiaty/{z}/{x}/{y}.png',{maxNativeZoom:11,maxZoom:13,minZoom:10,transparent:true}).addTo(map);
 
 const openStreet=L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 20,
